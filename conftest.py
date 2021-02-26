287a7b8c0ea3f27ac565f3376c6aef8d305fabf4
npletneva <natalia.pletneva@corp.badoo.com>
2021-02-26 13:10:46 +0300

    ipdated setup

--- conftest.py
+++ conftest.py
@@ -1,11 +1,24 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 import pytest
+
+
+# @pytest.fixture(scope="session")
+# def browser():
+#     driver = webdriver.Chrome(ChromeDriverManager().install())
+#     driver.implicitly_wait(10)
+#     driver.maximize_window()
+#     yield driver
+#     driver.close()
+#     driver.quit()
 
 
 @pytest.fixture(scope="session")
 def browser():
-    driver = webdriver.Chrome(ChromeDriverManager().install())
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
     driver.implicitly_wait(10)
     driver.maximize_window()
     yield driver
